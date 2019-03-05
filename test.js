40cadf3f24b36ae9bbe072ed27540b510574e118
Eduardo San Martin Morote <posva13@gmail.com>
2019-03-06 00:15:32 +0100

    test: add more

--- test.js
+++ test.js
@@ -11,8 +11,7 @@ describe('p-singleton', () => {
   })
 
   it('can be called multiple times', () => {
-    const promise = new Promise(() => {})
-    const p = pSingleton(() => promise)
+    const p = pSingleton(() => new Promise(() => {}))
     expect(p() instanceof Promise).toBe(true)
     expect(p() instanceof Promise).toBe(true)
     expect(p() instanceof Promise).toBe(true)
@@ -25,6 +24,17 @@ describe('p-singleton', () => {
     p().then(spy)
     expect(spy).not.toHaveBeenCalled()
     resolve('hey')
+    await tick()
+    expect(spy).toHaveBeenCalledWith('hey')
+  })
+
+  it('rejects the promise', async () => {
+    const [promise, resolve, reject] = fakePromise()
+    const p = pSingleton(() => promise)
+    const spy = jest.fn()
+    p().catch(spy)
+    expect(spy).not.toHaveBeenCalled()
+    reject('hey')
     await tick()
     expect(spy).toHaveBeenCalledWith('hey')
   })
@@ -46,7 +56,30 @@ describe('p-singleton', () => {
   it('consecutive calls return the same promise', () => {
     const [promise, resolve] = fakePromise()
     const p = pSingleton(() => promise)
-    const spy = jest.fn()
     expect(p()).toBe(p())
+  })
+
+  it('consecutive calls with different parameters return the same promise', () => {
+    const p = pSingleton(() => new Promise(() => {}))
+    expect(p()).toBe(p())
+    expect(p(2)).toBe(p(2))
+    expect(p(2, 3)).toBe(p(2, 3))
+    expect(p(2, 3)).not.toBe(p(2))
+    expect(p(2)).not.toBe(p(3))
+  })
+
+  it('returns a new promise if resolves', async () => {
+    let resolve
+    const p = pSingleton(() => {
+      const fake = fakePromise()
+      resolve = fake[1]
+      return fake[0]
+    })
+    const p1 = p()
+    resolve()
+    await tick()
+    const p2 = p()
+
+    expect(p1).not.toBe(p2)
   })
 })
